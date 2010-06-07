b41ffbf0903a877dffabbdf2bbc271be5e99b4f5
Kirill Nikitin <locke23rus@gmail.com>
2010-06-07 18:33:23 +0400

    Updated aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,18 +1,14 @@
-# cd
-alias .='cd ..'
-alias ..='cd ../..'
-alias ...='cd ../../..'
-
 # Redcar Editor
-alias redcar='redcar --theme=Railscasts --font=Monaco --font-size=12'
-alias redcar-dev='jruby $HOME/Projects/redcar/bin/redcar --font=Monaco --font-size=12'
+#alias redcar='jruby -S redcar'
+#alias redcar-dev='jruby $HOME/Projects/redcar/bin/redcar'
 
 # Reload zsh config
 alias reload='source ~/.zshrc'
 
 # Rubygems
-alias jgem='jruby -S gem'
-alias cheat='jruby -S cheat'
+#alias jgem='jruby -S gem'
+#alias cheat='jruby -S cheat'
 
 # Ruby on Rails
 alias r='rails'
+alias -g M=" -JT -m http://gist.github.com/raw/427726/69d72d9893a7902da092922f037cbf78cac35e06/rails3_template.rb"
