7d276d8d6caca112a8184e0cd9ef643345a6468d
Kirill Nikitin <locke23rus@gmail.com>
2015-11-25 04:05:42 +0300

    Uncomment LD_LIBRARY_PATH for rust

--- bash/paths.sh
+++ bash/paths.sh
@@ -1,4 +1,6 @@
+export PATH="$HOME/.dotfiles/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/.local/lib"
+
 # export GOPATH=$HOME/src/go
 # export PATH="$GOPATH/bin:$PATH"
 # export PATH="$PATH:$HOME/.local/share/elixir/bin"
-export PATH="$HOME/.dotfiles/bin:$PATH"
