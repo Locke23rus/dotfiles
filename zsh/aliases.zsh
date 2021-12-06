f77d7c6728243d4a2271db89d7713568f4676d98
Kirill Nikitin <locke23rus@gmail.com>
2021-12-06 16:52:00 +0200

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,6 +13,9 @@ alias ls='exa'
 alias reyarn='rm -rf node_modules/ && yarn'
 
 alias start_redis='redis-server /usr/local/etc/redis.conf'
+alias start_livebook='docker run -p 8080:8080 --pull always -v $(pwd):/data:z livebook/livebook'
+alias start_livebook_dropbox='docker run -p 8080:8080 --pull always -v $HOME/Dropbox/livebook:/data:z livebook/livebook'
 
 alias b='bundle'
 alias b-e='bundle exec'
+alias c='cargo'
