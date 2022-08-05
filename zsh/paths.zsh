a99a6a938d442c0de5731a17721ac3d7c2eb6e04
Kirill Nikitin <locke23rus@gmail.com>
2022-08-06 01:07:14 +0300

    update paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,17 +1,14 @@
-PATH="/usr/local/sbin:$PATH"
-PATH="/usr/local/bin:$PATH"
+PATH="/usrl/local/bin:$PATH"
 PATH="$PATH:$HOME/go/bin"
 PATH="$HOME/.dotfiles/bin:$PATH"
 PATH="$HOME/.cargo/bin:$PATH"
 PATH="$HOME/.mix/escripts:$PATH"
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
 MANPATH="/usr/local/man:$MANPATH"
-MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+MANPATH="/opt/homebrew/manpages:$MANPATH"
 
 export PATH
 export MANPATH
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
 
 export BAT_CONFIG_PATH="$HOME/.dotfiles/bat.conf"
 export LIVEBOOK_ROOT_PATH="$HOME/Dropbox/livebook"
