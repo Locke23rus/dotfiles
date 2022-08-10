554110a13ed435d8c112f1d94ac185a5a314d2cf
Kirill Nikitin <locke23rus@gmail.com>
2022-08-10 23:31:28 +0300

    Update readium

--- violentmonkey/readium.js
+++ violentmonkey/readium.js
@@ -1,14 +1,39 @@
 // ==UserScript==
 // @name        readium
+// @grant       GM_addStyle
 // ==/UserScript==
 
+GM_addStyle(`
+header,
+#credential_picker_container {
+  display: none!important;
+}
+`);
+
 if (document.querySelector('meta[property="og:site_name"][content="Medium"]')) {
-  let article = document.querySelector("article");
-  if (article) {
-    Array.from(article.parentNode.children).forEach((node) => {
-      if (node.tagName !== "ARTICLE") {
-        node.remove();
+  const removeSidebars = () => {
+    let main = document.querySelector("main");
+    if (main) {
+      Array.from(main.parentNode.children).forEach((node) => {
+        if (node.tagName !== "MAIN") {
+          node.remove();
+        }
+      });
+    }
+  };
+
+  const deleteDialog = () => {
+    let dialog = document.querySelector("div[role=alert]");
+    if (dialog) {
+      let parent = dialog;
+      while (parent.parentNode.tagName === "DIV") {
+        parent = parent.parentNode;
       }
-    });
-  }
+      parent.remove();
+    }
+  };
+
+  removeSidebars();
+  deleteDialog();
+  setTimeout(deleteDialog, 1000);
 }
