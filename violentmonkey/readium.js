4f09f074fdb303520e29f964977680da888f64c4
Kirill Nikitin <locke23rus@gmail.com>
2022-08-11 21:33:45 +0300

    reformat code

--- violentmonkey/readium.js
+++ violentmonkey/readium.js
@@ -4,7 +4,7 @@
 // ==/UserScript==
 
 if (document.querySelector('meta[property="og:site_name"][content="Medium"]')) {
-  GM_addStyle(`
+	GM_addStyle(`
 header,
 footer,
 #credential_picker_container {
@@ -12,41 +12,41 @@ footer,
 }
 `);
 
-  const removeSidebars = () => {
-    let main = document.querySelector("main");
-    if (main) {
-      Array.from(main.parentNode.children).forEach((node) => {
-        if (node.tagName !== "MAIN") {
-          node.remove();
-        }
-      });
-    }
-  };
+	const removeSidebars = () => {
+		let main = document.querySelector("main");
+		if (main) {
+			Array.from(main.parentNode.children).forEach((node) => {
+				if (node.tagName !== "MAIN") {
+					node.remove();
+				}
+			});
+		}
+	};
 
-  const removeDialog = () => {
-    let dialog = document.querySelector("div[role=alert]");
-    if (dialog) {
-      let parent = dialog;
-      while (parent.parentNode.tagName === "DIV") {
-        parent = parent.parentNode;
-      }
-      parent.remove();
-    }
-  };
+	const removeDialog = () => {
+		let dialog = document.querySelector("div[role=alert]");
+		if (dialog) {
+			let parent = dialog;
+			while (parent.parentNode.tagName === "DIV") {
+				parent = parent.parentNode;
+			}
+			parent.remove();
+		}
+	};
 
-  const removeFloatingActions = () => {
-    let action = document.querySelector(".pw-multi-vote-icon");
-    if (action) {
-      action.parentNode.parentNode.remove();
-    }
-  };
+	const removeFloatingActions = () => {
+		let action = document.querySelector(".pw-multi-vote-icon");
+		if (action) {
+			action.parentNode.parentNode.remove();
+		}
+	};
 
-  const removeEverything = () => {
-    removeSidebars();
-    removeDialog();
-    removeFloatingActions();
-  };
+	const removeEverything = () => {
+		removeSidebars();
+		removeDialog();
+		removeFloatingActions();
+	};
 
-  removeEverything();
-  setTimeout(removeEverything, 1000);
+	removeEverything();
+	setTimeout(removeEverything, 1000);
 }
