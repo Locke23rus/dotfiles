2be49521be4a0b4b71d9de182e82babea6485785
Kirill Nikitin <locke23rus@gmail.com>
2022-09-17 17:55:33 +0300

    Add script for using default browsers' monospace font

---
+++ violentmonkey/monospace-fonts.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        monospace-fonts
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+tt,
+code,
+samp {
+  font-family: monospace !important;
+}
+`);
