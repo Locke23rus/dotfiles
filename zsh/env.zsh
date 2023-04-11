49a6bf9dc202b25588a6563678d21904740765bc
Kirill Nikitin <locke23rus@gmail.com>
2023-04-11 17:03:44 +0300

    update aliases and envs

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,3 +3,8 @@ export RAILS_ENV=test
 export EDITOR=nvim
 export TERM=xterm-256color
 export MCFLY_FUZZY=5
+
+# docker
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
+export BUILDKIT_INLINE_CACHE=1
