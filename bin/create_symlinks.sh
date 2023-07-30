8497381c7accb907bb34d3d0dfce75518831e037
Kirill Nikitin <locke23rus@gmail.com>
2023-07-30 19:32:42 +0300

    Add script to create symlinks

---
+++ bin/create_symlinks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/helix ~/.config/helix
+ln -s ~/.dotfiles/home-manager/ ~/.config/home-manager
+ln -s ~/.dotfiles/irbrc ~/.irbrc
+ln -s ~/.dotfiles/nix/ ~/.config/nix
+ln -s ~/.dotfiles/nixpkgs/ ~/.config/nixpkgs
+ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
+ln -s ~/.dotfiles/wezterm.lua ~/.wezterm.lua
--- irbrc
+++ irbrc
@@ -3,4 +3,3 @@ require 'irb/ext/save-history'
 
 IRB.conf[:SAVE_HISTORY] = 1000
 IRB.conf[:HISTORY_FILE] = "#{ENV['HOME']}/.irb_history"
-
