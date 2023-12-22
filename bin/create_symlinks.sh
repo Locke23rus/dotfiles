6d1171b679db9de4417e59845c24e8e000cae5fc
Kirill Nikitin <locke23rus@gmail.com>
2023-12-23 00:57:55 +0200

    update nushell configs

--- bin/create_symlinks.sh
+++ bin/create_symlinks.sh
@@ -9,3 +9,6 @@ ln -s ~/.dotfiles/nix/ ~/.config/nix
 ln -s ~/.dotfiles/nixpkgs/ ~/.config/nixpkgs
 ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/.dotfiles/wezterm.lua ~/.wezterm.lua
+
+ln -s ~/.dotfiles/nu/env.nu ~/.config/nushell/env.nu
+ln -s ~/.dotfiles/nu/config.nu ~/.config/nushell/config.nu
