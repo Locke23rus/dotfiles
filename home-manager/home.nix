04eb5793118aeb546841580e76e728715d1b78c0
Kirill Nikitin <locke23rus@gmail.com>
2023-12-23 01:01:01 +0200

    Update home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -75,7 +75,7 @@
       enable = true;
       custom = "$HOME/.dotfiles/zsh";
       plugins = [
-        # "asdf"
+        "asdf"
         "bundler"
         "docker"
         "docker-compose"
