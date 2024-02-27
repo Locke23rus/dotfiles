35b3d510762df6b02438aedb29077f5f1cf6d3e1
Kirill Nikitin <locke23rus@gmail.com>
2024-02-27 15:48:31 +0200

    Update paths and aliases

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -7,6 +7,9 @@ PATH="$HOME/.cargo/bin:$PATH"
 PATH="$HOME/zig:$PATH"
 PATH="$HOME/.mix/escripts:$PATH"
 PATH="$HOME/.local/share/gem/ruby/3.2.0/bin:$PATH"
+# Extra paths for v1
+PATH="/opt/homebrew/opt/php@8.1/bin:$PATH"
+PATH="/opt/homebrew/opt/php@8.1/sbin:$PATH"
 
 MANPATH="/usr/local/man:$MANPATH"
 MANPATH="/opt/homebrew/manpages:$MANPATH"
